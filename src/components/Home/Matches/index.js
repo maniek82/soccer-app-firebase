9f77b354b1561b9a5f397e9c0db69b8db37c88a0
maniek82 <mariusz.tripler@gmail.com>
2018-11-19 09:17:30 +0000

    featured animation finished

---
+++ src/components/Home/Matches/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {Tag} from '../../ui/misc';
+
+const MatchesHome = () => {
+    return (
+        <div className="home_matches_wrapper">
+            <div className="container">
+                <Tag
+                    bck = "#0e1731"
+                    size="50px"
+                    color="#fff"
+                    add={{
+                        color: 'red'
+                    }}
+                >
+                  Matches  
+                </Tag>
+                block  
+               <Tag
+                    bck = "#fff"
+                    size="20px"
+                    color="#0e1731"
+                    link={true}
+                    linkTo="/the_team"
+                    
+               >
+                   See more matches
+               </Tag>
+            </div>
+            
+        </div>
+    );
+};
+
+export default MatchesHome;
